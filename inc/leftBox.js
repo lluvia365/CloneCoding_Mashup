4160bb849270d0ff805d7add70684eb70e8ad246
lluvia365 <62463505+lluvia365@users.noreply.github.com>
2021-07-23 01:01:35 +0900

    1

---
+++ inc/leftBox.js
@@ -0,0 +1,15 @@
+$(function(){  // 페이지가 열리면
+            
+        // view 버튼으로 메뉴 보이기/숨기기
+        $('#view').click(function(){
+            $('#leftBox').fadeToggle();  // 천천히 열리고 닫히게 
+        });
+ 
+        
+    });  // $(function()
+
+
+
+
+
+    
